2c52acf4adfe71a7d32b28d8924433d9d79e5a7b
AKoivisto <antton.v.koivisto@student.jyu.fi>
2025-01-14 11:34:32 +0200

    5.13 - 5.14

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -16,23 +16,29 @@ const Blog = ({ blog, updateLikes, deleteBlog, user }) => {
 
   return (
     <div>
-      <div style={{ ...hideWhenVisible, ...blogstyle }}>
-        {blog.title} {blog.author}
-        <button style={{ marginLeft: '5px' }} onClick={() => handleShowDetails(true)}>view</button>
-      </div>
-      <div style={{ ...showWhenVisible, ...blogstyle }}>
-        {blog.title} {blog.author}
-        <button style={{ marginLeft: '5px' }} onClick={() => handleShowDetails(false)}>hide</button>
-        <div>{blog.url}</div>
-        <div>
-          likes {blog.likes}
-          <button style={{ marginLeft: '5px' }} onClick={() => updateLikes(blog)}>like</button>
-        </div>
-        {blog.user.name}
-        {blog.user.username === user.username && (
+      <div style={blogstyle}>
+        {! showDetails ? (
           <div>
-            <button style={{ marginLeft: '5px' }} onClick={() => deleteBlog(blog)}>delete blog</button>
+            {blog.title} {blog.author}
+            <button style={{ marginLeft: '5px' }} onClick={() => handleShowDetails(true)}>view</button>
           </div>
+        ) : (
+          <div>
+            {blog.title} {blog.author}
+            <button style={{ marginLeft: '5px' }} onClick={() => handleShowDetails(false)}>hide</button>
+            <div>{blog.url}</div>
+            <div>
+              likes {blog.likes}
+              <button style={{ marginLeft: '5px' }} onClick={() => updateLikes(blog)}>like</button>
+            </div>
+            {blog.user.name}
+            {blog.user.username === user.username && (
+              <div>
+                <button style={{ marginLeft: '5px' }} onClick={() => deleteBlog(blog)}>delete blog</button>
+              </div>
+            )}
+          </div>
+
         )}
       </div>
     </div>
---
+++ src/components/Blog.test.jsx
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
+import { beforeEach } from 'vitest'
+
+let component
+
+const blog = {
+  title: 'Blog test title',
+  author: 'Bruno Blogger',
+  url: 'https://www.blogtests.com/bruno',
+  likes: 0,
+  user : {
+    username: 'testuser',
+    name: 'Tero Testailija'
+  }
+}
+
+const user = {
+  username: 'testuser',
+  name: 'Tero Testailija'
+}
+
+const mockUpdateLikes = vi.fn()
+const mockDeleteBlog = vi.fn()
+
+beforeEach(() => {
+  component = render(<Blog blog={blog} updateLikes={mockUpdateLikes} user={user} deleteBlog={mockDeleteBlog}/>)
+})
+
+test('renders only title', () => {
+
+  // screen.debug()
+
+  expect(component.container).toHaveTextContent(blog.title)
+  expect(component.container).not.toHaveTextContent(blog.url)
+})
+
+test('displays url, likes and user when the view button is clicked', async () => {
+
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  screen.debug()
+
+  expect(component.container).toHaveTextContent(blog.title)
+  expect(component.container).toHaveTextContent(blog.url)
+  expect(component.container).toHaveTextContent(blog.likes)
+  expect(component.container).toHaveTextContent(blog.user.name)
+})
