a674feb36de44fdc11b7ecbf73841dd6026599b8
AKoivisto <antton.v.koivisto@student.jyu.fi>
2025-01-14 11:38:07 +0200

    5.15

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -43,10 +43,23 @@ test('displays url, likes and user when the view button is clicked', async () =>
   const button = screen.getByText('view')
   await user.click(button)
 
-  screen.debug()
+  // screen.debug()
 
   expect(component.container).toHaveTextContent(blog.title)
   expect(component.container).toHaveTextContent(blog.url)
   expect(component.container).toHaveTextContent(blog.likes)
   expect(component.container).toHaveTextContent(blog.user.name)
+})
+
+test('Event handler is called twice when the like button is clicked twice', async () => {
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockUpdateLikes.mock.calls).toHaveLength(2)
 })
